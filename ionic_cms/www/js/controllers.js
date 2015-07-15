92ef8e7449a7c5af78cacfa97728383c501ac739
Omayeli Arenyeka <oa495@nyu.edu>
2015-07-15 18:05:03 -0400

    fixing sharing

--- ionic_cms/www/js/controllers.js
+++ ionic_cms/www/js/controllers.js
@@ -274,4 +274,77 @@ angular.module('starter.controllers', [])
   }
   $scope.load()
 
-}]);
+}])
+
+.controller("ShareCtrl", function($scope, $cordovaSocialSharing) {
+    $scope.shareAnywhere = function() {
+        $cordovaSocialSharing.share("My Demo Message", "My Demo Subject", "www/img/image1.jpeg",    "http://santoshshinde2012.blogspot.com");
+    }
+   $scope.shareViaTwitter = function(message, image, link) {
+    console.log("Twitter");
+     $cordovaSocialSharing
+       .shareViaTwitter(message, image, link)
+       .then(function(result) {
+         // Success!
+         alert("success : "+result);
+       }, function(err) {
+         // An error occurred. Show a message to the user
+         alert("Cannot share on Twitter");
+       });
+   }
+
+
+   $scope.shareViaWhatsApp = function(message, image, link) {
+    console.log("WhatsApp");
+     $cordovaSocialSharing
+       .shareViaWhatsApp(message, image, link)
+       .then(function(result) {
+        alert(result);
+         // Success!
+       }, function(err) {
+         // An error occurred. Show a message to the user
+          alert("Cannot share on WhatsApp");
+       });
+   }
+   $scope.shareViaFacebook = function(message, image, link) {
+    console.log("facebook");
+     $cordovaSocialSharing
+       .shareViaFacebook(message, image, link)
+       .then(function(result) {
+         // Success!
+       }, function(err) {
+         // An error occurred. Show a message to the user
+         alert("Cannot share on Facebook");
+       });
+   }
+  // access multiple numbers in a string like: '0612345678,0687654321'
+   $scope.shareViaSMS = function(message, number) {
+     $cordovaSocialSharing
+       .shareViaSMS(message, number)
+       .then(function(result) {
+        alert(result);
+         // Success!
+       }, function(err) {
+         // An error occurred. Show a message to the user
+       });
+   }
+  // TO, CC, BCC must be an array, Files can be either null, string or array
+   $scope.shareViaFacebook = function(message, subject, toArr, bccArr, file) {
+     $cordovaSocialSharing
+       .shareViaEmail(message, subject, toArr, bccArr, file)
+       .then(function(result) {
+         // Success!
+       }, function(err) {
+         // An error occurred. Show a message to the user
+       });
+   }
+   $scope.shareViaFacebook = function(socialType, message, image, link) {
+    $cordovaSocialSharing
+      .canShareVia(socialType, message, image, link)
+      .then(function(result) {
+        // Success!
+      }, function(err) {
+        // An error occurred. Show a message to the user
+      });
+    }
+});
