f40a60fa7982d95a3114de4d466bffc0a3f56503
Omayeli Arenyeka <oa495@nyu.edu>
2015-07-16 11:32:37 -0400

    finishing up social sharing

--- ionic_cms/www/js/controllers.js
+++ ionic_cms/www/js/controllers.js
@@ -278,7 +278,7 @@ angular.module('starter.controllers', [])
 
 .controller("ShareCtrl", function($scope, $cordovaSocialSharing) {
     $scope.shareAnywhere = function() {
-        $cordovaSocialSharing.share("My Demo Message", "My Demo Subject", "www/img/image1.jpeg",    "http://santoshshinde2012.blogspot.com");
+        $cordovaSocialSharing.share("Hey check out this cool app for putting content on the MTA Kiosk!", "Cool Contest!", "img/GoogleWhite.png", "http://www.intern-cms-dev.elasticbeanstalk.com");
     }
    $scope.shareViaTwitter = function(message, image, link) {
     console.log("Twitter");
@@ -293,19 +293,6 @@ angular.module('starter.controllers', [])
        });
    }
 
-
-   $scope.shareViaWhatsApp = function(message, image, link) {
-    console.log("WhatsApp");
-     $cordovaSocialSharing
-       .shareViaWhatsApp(message, image, link)
-       .then(function(result) {
-        alert(result);
-         // Success!
-       }, function(err) {
-         // An error occurred. Show a message to the user
-          alert("Cannot share on WhatsApp");
-       });
-   }
    $scope.shareViaFacebook = function(message, image, link) {
     console.log("facebook");
      $cordovaSocialSharing
@@ -317,34 +304,4 @@ angular.module('starter.controllers', [])
          alert("Cannot share on Facebook");
        });
    }
-  // access multiple numbers in a string like: '0612345678,0687654321'
-   $scope.shareViaSMS = function(message, number) {
-     $cordovaSocialSharing
-       .shareViaSMS(message, number)
-       .then(function(result) {
-        alert(result);
-         // Success!
-       }, function(err) {
-         // An error occurred. Show a message to the user
-       });
-   }
-  // TO, CC, BCC must be an array, Files can be either null, string or array
-   $scope.shareViaFacebook = function(message, subject, toArr, bccArr, file) {
-     $cordovaSocialSharing
-       .shareViaEmail(message, subject, toArr, bccArr, file)
-       .then(function(result) {
-         // Success!
-       }, function(err) {
-         // An error occurred. Show a message to the user
-       });
-   }
-   $scope.shareViaFacebook = function(socialType, message, image, link) {
-    $cordovaSocialSharing
-      .canShareVia(socialType, message, image, link)
-      .then(function(result) {
-        // Success!
-      }, function(err) {
-        // An error occurred. Show a message to the user
-      });
-    }
 });
